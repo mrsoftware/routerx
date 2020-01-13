10d6357f0f43ec03669fdc54e851c42bf0cc0f38
Mohammad Rajabloo <themrsoftware@gmail.com>
2020-01-13 18:48:37 +0330

    Export layouts and Route class

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import Route from './Route';
+import Component from './layouts/Component';
+import BottomTabs from './layouts/BottomTabs';
+import Stack from './layouts/Stack';
+
+export {
+	Route,
+	Component,
+	BottomTabs,
+	Stack,
+};
